209875b55882383499a70bb1287801bc34cd1d0c
長瀬洋二 <y-nagase@localhost.local>
2024-06-14 19:29:42 +0900

    update: checkbox feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+from datetime import time, datetime
 
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-st.write(df2)
+st.header('st.checkbox')
 
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more ??")
+
+if coffee: 
+     st.write("Okay, here's some coffee ?")
+
+if cola:
+     st.write("Here you go ??")
+
+# df2 = pd.DataFrame(
+#      np.random.randn(200, 3),
+#      columns=['a', 'b', 'c'])
+# st.write(df2)
+# st.line_chart(df2)
+
+
+# c = alt.Chart(df2).mark_circle().encode(
+#      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+# st.write(c)
+
+# df3 = pd.read_csv("data.csv")
+# st.write(df3)
