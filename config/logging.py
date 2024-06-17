56dcad5772c8888d7a4f63e61a2677853f6c6d4a
長瀬洋二 <y-nagase@localhost.local>
2024-06-18 00:01:06 +0900

    deploy

--- config/logging.py
+++ config/logging.py
@@ -5,7 +5,7 @@ def setup():
         format='%(asctime)s - %(message)s',
         level=logging.INFO,
         handlers=[
-            logging.FileHandler("log/app.log"),
+            # logging.FileHandler("log/app.log"),
             logging.StreamHandler()
         ]
     )
