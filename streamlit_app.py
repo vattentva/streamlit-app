ce012c9cda87ecf122e3bc9fb35f7df50a3447f5
vattentva <vattentva.d3@gmail.com>
2024-08-12 18:00:45 +0900

    update: custom css

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,11 @@ import streamlit as st
 
 # カスタムCSSを適用
 hide_streamlit_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    header {visibility: hidden;}
-    footer {visibility: hidden;}
-    </style>
-    """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+<style>
+.stActionButton {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 sidebar = {
     "Streamlit": [
